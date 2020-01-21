c0809da66aef4cfba7b26816424b97dc598b4dbd
shaigue <shaigue@gmail.com>
2020-01-21 17:56:52 +0200

    visualizations & merge

---
+++ simulation/Models.py
@@ -0,0 +1,7 @@
+"""This is a class that represents the observation Models."""
+# might we want something like this?
+# model(M, N)
+# belief = model.predict()
+# model.add_observation(x,y,Z)
+# model.train()
+# model.eval()?
